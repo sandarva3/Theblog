8972aa4c5c19f6251c0299f1fe5dd9adadf116b2
Sandarva3 <Sandarva_email>
2022-12-25 19:13:00 +0545

    changed static file name

--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 
