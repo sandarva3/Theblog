5f86990bf0c00b5635ec39188294d62f857e8fb9
Sandarva3 <Sandarva_email>
2022-12-25 18:52:24 +0545

    changed the build line in setting

--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -13,7 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#commented the line below
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
